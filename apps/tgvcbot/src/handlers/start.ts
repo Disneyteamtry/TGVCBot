86825dc997c4b39ccc61f37ac51e3c9d509d708c
Disneyteamtry <90768794+Disneyteamtry@users.noreply.github.com>
2022-05-05 06:51:43 +0530

    Update start.ts

--- apps/tgvcbot/src/handlers/start.ts
+++ apps/tgvcbot/src/handlers/start.ts
@@ -21,12 +21,12 @@ composer.command('start', (ctx) => {
     `Hi <a href="tg://user?id=${ctx.from.id}">${escape(
       ctx.from.first_name + ' ' + ctx.from.last_name
     )}</a>\n` +
-    `I Play Songs in Group Voice Chats.\n` +
-    `If you like this bot consider adding a star to the <a href="https://github.com/ArnabXD/TGVCBot">repository</a>`;
+    `I Am Shiny Hunts  Play Song Bot in Group Voice Chats.\n` +
+    `If you like this bot consider adding a star to the <a href="https://t.me/mewtwolover">Telegram</a>`;
   return ctx.reply(text, {
     reply_markup: new InlineKeyboard().url(
       'View Source',
-      'https://github.com/ArnabXD/TGVCBot'
+      'https://github.com'
     ),
     disable_web_page_preview: true,
     parse_mode: 'HTML'
